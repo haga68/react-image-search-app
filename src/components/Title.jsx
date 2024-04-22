8a5b5f09dd3bd0b72d9a42d584ec83e835e4f1f0
haga68 <gaah68@hotmail.co.jp>
2024-04-22 15:19:33 +0800

    first commit

---
+++ src/components/Title.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Title = () => {
+  return (
+    <header>
+        <h1>Image Search App</h1>
+        <p>By <a href="https://unsplash.com/">Unsplash</a></p>
+    </header>
+  )
+}
+
+//rafce変換は便利
+
+export default Title
